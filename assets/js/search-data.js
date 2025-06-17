66189c2e31429281fa3f8543a289616067629e70
fuxiaoji <1105793864@qq.com>
2025-06-17 13:08:40 +0000

    Deploying to gh-pages from @ fuxiaoji/fuxiaoji.github.io@2a829ccc7a4bd7a26fe5382151df7f36b3593651 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,13 +3,20 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-about",
+          title: "about",
+          description: "个人简介",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/about/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
